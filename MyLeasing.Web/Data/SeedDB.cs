3da2dec48d480d70c43e8c2a14ded6f6d5f2ecd5
cgeronimo <48395675+cgeronimo@users.noreply.github.com>
2020-02-24 22:28:08 -0400

    Se creo Seed Task

---
+++ MyLeasing.Web/Data/SeedDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+	public class SeedDB
+	{
+		private readonly DataContext _context;
+
+		public SeedDB(DataContext context)
+		{
+			_context = context;
+		}
+
+		public async Task SeedAsync()
+		{
+			await _context.Database.EnsureCreatedAsync();
+			await CheckPropertyTypesAsync();
+		}
+
+		private async Task CheckPropertyTypesAsync()
+		{
+			if (!_context.PropertyTypes.Any())
+			{
+				_context.PropertyTypes.Add(new Entities.PropertyType { Name = "Apartamento" });
+				_context.PropertyTypes.Add(new Entities.PropertyType { Name = "Casa" });
+				_context.PropertyTypes.Add(new Entities.PropertyType { Name = "Negocio" });
+				await _context.SaveChangesAsync();
+			}
+		}
+	}
+}
